60beee2fb3fcecaa721ba4acc06e207a6e2a6635
kxz-ubuntu <15068701650@163.com>
2020-07-07 14:11:17 +0800

    initialize tagging

--- src/tagging.py
+++ src/tagging.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+"""save messages with corresponding tags and answer questions from users,
+   display and cleaning function are also provided"""
+from wechaty import Message, Contact, Room
+from wechaty.plugin import WechatyPlugin
 
+class Tagging(WechatyPlugin):
+    """tagging system plugin for bot"""
+    @property
+    def name(self) -> str:
+        """get the name of the plugin"""
+        return 'tagging'
+
+    async def on_message(self, msg: Message):
+        """listen message event"""
+        from_contact = msg.talker()
+        text = msg.text()
+        room = msg.room()
