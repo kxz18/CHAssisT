4958ed24d5da7d50a4e7c16119e2a611f8916d5d
kxz-Manjaro <15068701650@163.com>
2020-07-07 20:27:17 +0800

    initialize #3

--- src/data/msg_with_tag.py
+++ src/data/msg_with_tag.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+"""The data structure of messages with tags"""
+
+class MsgWithTags:
+    """message with tag"""
+    def __init__(self, quoted, reply):
+        self.quoted: str = quoted
+        self.tags = MsgWithTags.getTagsFromMsg(reply)
 
