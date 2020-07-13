f0fe49e9337ebeffb7c895540cbcc314815ef7a3
kxz-ubuntu <15068701650@163.com>
2020-07-13 16:58:47 +0800

    finish unit test of question answering #4

--- tests/test_question_answering.py
+++ tests/test_question_answering.py
@@ -17,6 +17,7 @@ MSG_CONTENTS = [('今天19点开会', '今天开会时间'),
                 ('停车场在6号楼东侧', '停车位置'),
                 ('今天我爆炸了', '李总今天的活动'),
                 ('微信号容易被封', '新手需要注意的地方')]
+
 def init_data():
     """create tag_controller"""
     for path in [PATH, CONFIG]:
@@ -28,24 +29,31 @@ def init_data():
     for quoted, tag, in MSG_CONTENTS:
         msg = MsgWithTag(quoted, tag, 'me')     # talker is not important here
         interface.save_msg(msg)
+    interface.save()
+    return interface
 
 def test_confidence_high():
     """test answer with high confidence"""
-    interface = DataTransfer(Database(PATH, CONFIG))
+    interface = init_data()
     question_answering = QuestionAnswering(interface)
     assert question_answering.handle_msg('今天几点开会？', True)
     assert question_answering.get_reply() == MSG_CONTENTS[0][0]
+    assert question_answering.handle_msg('问卷几点截止？', True)
+    assert question_answering.get_reply() == MSG_CONTENTS[1][0]
+    assert question_answering.handle_msg('请问停车位置在哪？', True)
+    assert question_answering.get_reply() == MSG_CONTENTS[2][0]
 
 def test_confidence_median():
     """test answer with median confidence"""
-    interface = DataTransfer(Database(PATH, CONFIG))
+    interface = init_data()
     question_answering = QuestionAnswering(interface)
-
+    assert question_answering.handle_msg('我车停哪', True)
+    assert question_answering.get_reply() == reply.similar_answer_help([MSG_CONTENTS[2][1]])
 
 def test_confidence_no():
     """test situation in which no answer was found"""
-    interface = DataTransfer(Database(PATH, CONFIG))
+    interface = init_data()
     question_answering = QuestionAnswering(interface)
-    assert not question_answering('hey man', False)
-    assert question_answering('hey man', True)
+    assert not question_answering.handle_msg('hey man', False)
+    assert question_answering.handle_msg('hey man', True)
     assert question_answering.get_reply() == reply.no_answer_found()
