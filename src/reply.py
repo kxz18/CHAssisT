cea8724460b46018bd70e015e9dd31b8bc531265
kongxiangzhe_s20_236 <15068701650@163.com>
2020-07-16 10:57:53 +0800

    modify reply message #15

--- src/reply.py
+++ src/reply.py
@@ -10,6 +10,24 @@ def del_msg_success(_id):
     """successfully deleted a message"""
     return f'Deleted #{_id}'
 
+def set_timed_delete_success(params, delta):
+    """successfully set timed delete job
+    params:
+        params: dictionary of time parameters of the job
+        delta: messages that are 'delta' days before will be deleted"""
+    time_point = ''
+    for key in params:
+        if params[key] != '*':
+            time_point += f'{key} {params[key]}'
+    return f'messages that are {delta} days before will be deleted on every {time_point}'
+
+def stop_timed_delete(success):
+    """return reply of stopping timed delete according to success status"""
+    if success:
+        return 'Successfully stoped timed delete'
+    else:
+        return 'No timed delete task running'
+
 # Question Answering
 def no_answer_found():
     """cannot find any answer"""
