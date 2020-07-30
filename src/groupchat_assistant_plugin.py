bad756c0293a4fc2c9ecacb3df181bd9f5f94939
kongxiangzhe_s20_236 <15068701650@163.com>
2020-07-30 20:01:51 +0800

    add four plugins #31

--- src/groupchat_assistant_plugin.py
+++ src/groupchat_assistant_plugin.py
@@ -2,8 +2,15 @@
 # -*- coding:utf-8 -*-
 """plugin of groupchat assistant, including tagging, member manager,
 help system and timed task plugin"""
+from wechaty import Wechaty
 from wechaty.plugin import WechatyPlugin
 from wechaty_puppet import get_logger
+
+from tagging_plugin import Tagging
+from timed_task_plugin import TimedTask
+from member_manager_plugin import MemberManager
+from help_plugin import Help
+from help_modules.example_dict import groupchat_bot_help_zh
 
 log = get_logger('Groupchat Assistant')
 
@@ -20,4 +27,14 @@ class GroupchatAssistant(WechatyPlugin):
             data_path: path of database file,
             config_path: path of database config file, which
                          contains auto-created information"""
-        self.tagging = 
+        self.plugins = [Tagging(data_path, config_path),
+                        TimedTask(),
+                        MemberManager(),
+                        Help('帮助', '#', groupchat_bot_help_zh)]
+
+    async def init_plugin(self, wechaty: Wechaty):
+        """override init_plugin function. Add member plugin to bot"""
+        self.bot = wechaty
+        self.bot.use(self.plugins)
+        for plugin in self.plugins:
+            plugin.init_plugin()    # manually initialize their bots
